8fb20f56d4aa66aacefbf7c096a622cfd73909ab
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-11-16 15:24:27 -0500

    Implemented decimal point

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 app = angular.module('calculatorApp', []);
 
 app.controller('calcCtrl', function($scope) {
-    $scope.displayValue = 0;
+    $scope.displayValue = '0';
 
 
     var displayValueInitialized = false,
+        isFloat = false,
+        isInt = function(number) { return number % 1 === 0},
         mathExpression = '';
 
     $scope.numberClick = function(value) {
@@ -19,16 +21,24 @@ app.controller('calcCtrl', function($scope) {
         displayValueInitialized = false;
     };
 
+    $scope.addDecimal = function() {
+        if (!displayValueInitialized) $scope.displayValue = '.';
+        else if (!isFloat) $scope.displayValue += '.';
+        isFloat = true;
+        displayValueInitialized = true;
+    };
+
     $scope.evaluateExpression = function() {
         mathExpression += $scope.displayValue + event.srcElement.value;
         $scope.displayValue = Parser.parse(mathExpression).evaluate();
+        if (isInt($scope.displayValue)) isFloat = false;
         mathExpression = '';
         displayValueInitialized = false;
     };
 
     $scope.resetCalculator = function() {
         mathExpression = '';
-        $scope.displayValue = 0;
+        $scope.displayValue = '0';
         displayValueInitialized = false;
     };
 });
