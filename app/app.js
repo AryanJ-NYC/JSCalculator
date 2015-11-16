333067c5aca6d3e69217be381eecdeeec084e87d
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-11-16 10:30:10 -0500

    Initial values display correctly on click of number

--- app/app.js
+++ app/app.js
@@ -2,4 +2,9 @@ app = angular.module('calculatorApp', []);
 
 app.controller('calcCtrl', function($scope) {
     $scope.displayValue = 0;
+
+    $scope.numberClick = function(value) {
+        if ($scope.displayValue == 0) $scope.displayValue = value;
+        else $scope.displayValue += value;
+    };
 });
