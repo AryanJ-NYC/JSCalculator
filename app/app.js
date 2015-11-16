6b74a047a8b1597da9c40ebdbf6fb5f783ba69cb
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-11-16 11:01:28 -0500

    Equal button working on first press

--- app/app.js
+++ app/app.js
@@ -2,9 +2,27 @@ app = angular.module('calculatorApp', []);
 
 app.controller('calcCtrl', function($scope) {
     $scope.displayValue = 0;
+    $scope.mathExpression = ''; // TODO Use parser.js (https://github.com/silentmatt/js-expression-eval/tree/master) to parse expression
+
+    var displayValueInitialized = false;
 
     $scope.numberClick = function(value) {
-        if ($scope.displayValue == 0) $scope.displayValue = value;
-        else $scope.displayValue += value;
+        if (!displayValueInitialized) {
+            $scope.displayValue = value;
+            displayValueInitialized = true;
+        } else $scope.displayValue += value;
+    };
+
+    $scope.operatorClick = function(event) {
+        $scope.mathExpression += $scope.displayValue + event.srcElement.value;
+        displayValueInitialized = false;
+        console.log($scope.mathExpression);
+    };
+
+    $scope.evaluateExpression = function() {
+        $scope.mathExpression += $scope.displayValue + event.srcElement.value;
+        console.log($scope.mathExpression);
+        $scope.displayValue = Parser.parse($scope.mathExpression).evaluate();
+        displayValueInitialized = false;
     };
 });
